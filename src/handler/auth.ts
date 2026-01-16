c5a7b616456942a2274a3f66786a37df260011ab
Abinash Parida <abinashparida2021@gmail.com>
2026-01-16 14:55:19 +0530

    refactor(auth): hashes password securely

--- src/handler/auth.ts
+++ src/handler/auth.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import type {Socket} from "socket.io";
 import type {IEmployee} from "../interface/employee.ts";
 import type {IDepartment} from "../interface/department.ts";
@@ -16,21 +17,21 @@ async function authSignUp(socket: Socket, employee: IEmployee) {
     if (await isEmployeeExists(email)) {
         return socket.emit("sign_up_response", { status: "failed", message: "employee already exists with this email."});
     }
+    employee.password = await bcrypt.hash(employee.password, 10);
     await addNewEmployee(employee);
     const token = signToken({email});
     return socket.emit("sign_up_response", { status: "success", message: message.message, token });
 }
 async function authSignIn(socket: Socket, employee: IEmployee) {
     const message = validateAuthCredentials(employee, false);
-    if (!message.status) {
-        return socket.emit("sign_in_response", { status: "failed", message: message.message});
+    if (!message.status) return socket.emit("sign_in_response", { status: "failed", message: message.message});
+    const employeeData: IEmployee | undefined = await getEmployeeDataByEmail(employee.email);
+    if (!employeeData) return socket.emit("sign_in_response", { status: "failed", message: "employee not found with this email"});
+    if (!await bcrypt.compare(employee.password, employeeData.password)) {
+        return socket.emit("sign_in_response", { status: "failed", message: "invalid password."});
     }
-    const email = employee.email as string;
-    if (await isEmployeeExists(email)) {
-        const token = signToken({email});
-        return socket.emit("sign_in_response", { status: "success", message: message.message, token});
-    }
-    return socket.emit("sign_in_response", { status: "failed", message: "employee not found with this email."});
+    const token = signToken({email: employee.email});
+    return socket.emit("sign_in_response", { status: "success", message: message.message, token});
 }
 async function authVerification(socket: Socket, next: (err?: ExtendedError) => void) {
     try {
@@ -44,7 +45,7 @@ async function authVerification(socket: Socket, next: (err?: ExtendedError) => v
         if (!employee) return next(new Error("Employee not found."));
 
         if(employee.role === "admin" && adminPassword) {
-            if(adminPassword === "me nahi bataunga") socket.data.role = "admin"
+            if(adminPassword === process.env.ADMIN_PASSWORD) socket.data.role = "admin"
             else return next(new Error("invalid admin password."));
         } else {
             const department: IDepartment|null = await getDepartment(employee.departmentId.toString());
